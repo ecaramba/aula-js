5a4ac3ceeaa4c1132004a6d76a0246870bb11342
Prof Edir <ecaramba@gmail.com>
2023-09-15 21:29:45 -0300

    add form pokedex

--- aula/src/Pokedex.js
+++ aula/src/Pokedex.js
@@ -1,7 +1,35 @@
 
 export default function Pokedex()
 {
-    return (<div>
-        <h1>Pokedex</h1>
+    return (
+    <div className="container">
+
+        <div className="row justify-content-md-center">
+            <div className="col-6 mt-4" >
+
+                <div className="input-group">
+                    <input type="text" placeholder="Nome ou ID" className="form-control"/>
+                    <button className="btn btn-outline-danger" type="button">Pesquisar</button>
+                </div>
+
+                <div className="card text-bg-danger mt-4">
+                    <div className="card-header">Pokedex</div>
+                    <img src="https://placehold.co/300x200" />
+                    
+                    <div className="card-body">
+                        <h5 className="card-title">Nome do bichinho</h5>
+                        <p className="card-text">
+                       
+                        </p>
+                    </div>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">peso</li>
+                        <li className="list-group-item">altura</li>
+                        <li className="list-group-item">Habilidade</li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
     </div>)
 }
