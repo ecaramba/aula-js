1f9663cee62689fb7b7e2131d374faf48e6164bc
Prof Edir <ecaramba@gmail.com>
2023-09-19 20:31:19 -0300

    add router

--- aula/src/Links.js
+++ aula/src/Links.js
@@ -1,24 +1,13 @@
-import Pokedex from './Pokedex';
-import Ola from './Ola';
+
 
 export default function Links() {
-    let component = "";
-
-    if (window.location.hash === '#pokedex')
-    {
-        component = <Pokedex />;
-    } else if (window.location.hash === "#ola")
-    {
-        component = <Ola />
-    }
 
     return (
         <div>
-            | <a href="#pokedex">Pokedex</a> 
-            | <a href="#cidades">Cidades</a> 
-            | <a href="#ola">Ola</a> 
+            | <a href="/pokedex">Pokedex</a> 
+            | <a href="/cidades">Cidades</a> 
+            | <a href="/ola">Ola</a> 
 
-            {component}
 
         </div>
         
--- aula/src/index.js
+++ aula/src/index.js
@@ -3,17 +3,42 @@ import ReactDOM from 'react-dom/client';
 
 import "bootstrap/dist/css/bootstrap.css"
 
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
 
 import Links from './Links';
+import Pokedex from './Pokedex';
+import Ola from './Ola';
+import Cidades from './Cidades'
+
+const rotas = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1>Exemplos feitos em aula</h1>
+    },
+    {
+        path: "/pokedex",
+        element: <Pokedex />
+    }, 
+    {
+        path: "/ola",
+        element: <Ola />
+    },
+    {
+        path: "/cidades",
+        element: <Cidades />
+    }
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-
-
 root.render(
     <div>
         <Links />
         
+        <RouterProvider router={ rotas }></RouterProvider>
 
     </div>
 );
