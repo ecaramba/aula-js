8d46c691b75dc2c9d729bc56dcfd3e20de865aee
Prof Edir <ecaramba@gmail.com>
2023-09-19 21:36:26 -0300

    listagem da cidade

--- aula/src/Cidades.js
+++ aula/src/Cidades.js
@@ -4,11 +4,22 @@ import { useState } from 'react';
 export default function Cidades()
 {
     const [cidades, setCidade] = useState(["Curitiba", "Colombo", "Pinhais"]);
-    const [nova, setNova] = useState();
+    const [nova, setNova] = useState("");
+    const [pesquisa, setPesquisa] = useState("");
 
     // gerando os li para cada item do vetor
-    let lista = cidades.map(function(item){
-        return <li key={ item }>{ item }</li>
+    let lista = cidades
+            // filtra os elementos do vetor pelo conteudo digitado
+            .filter((item) => {
+                return item
+                    // coloca para minisculo para pesuisar todos
+                    .toLowerCase()
+                    // pesquisa cidade começando com o texto digitado
+                    .startsWith(pesquisa.toLowerCase());
+            })
+            // adiciona li em cada item do vetor
+            .map(function(item){
+                return <li key={ item }>{ item }</li>
     })
 
     // adiciona cidade no vetor cidades
@@ -32,9 +43,13 @@ export default function Cidades()
     return (
         <div>
             <h1>Lista de Cidades</h1>
-            <input type="text" onChange={ ler } />
+            <input type="text" onChange={ ler } className="form-control" />
 
-            <button onClick={ add }>Adicionar Cidade</button>
+            <button onClick={ add } className='btn btn-primary'>Adicionar Cidade</button>
+            <br />
+
+            <label>Pesquisa</label>
+            <input type='text' onChange={ (ev) => setPesquisa(ev.target.value) } className="form-control" />
 
             <div className="lista">
                 <ul>
